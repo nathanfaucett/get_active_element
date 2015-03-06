39e75c715b2795315cab37c28399cfb2a29d526e
Nathan Faucett <nathanfaucett@gmail.com>
2015-03-06 08:48:16 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+get_active_element
+=======
+
+get active element for the browser and node.js
---
+++ src/index.js
@@ -0,0 +1,16 @@
+var isDocument = require("is_document"),
+    environment = require("environment");
+
+
+var document = environment.document;
+
+
+module.exports = function getActiveElement(ownerDocument) {
+    ownerDocument = isDocument(ownerDocument) ? ownerDocument : document;
+
+    try {
+        return ownerDocument.activeElement || ownerDocument.body;
+    } catch (e) {
+        return ownerDocument.body;
+    }
+};
